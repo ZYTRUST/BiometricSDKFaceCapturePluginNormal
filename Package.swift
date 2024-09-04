36b898d9cde4bebb7c597e34dc37eb7c8062a796
Iván Cáceres <ztmobile@zytrust.com>
2024-09-04 14:35:55 -0500

    4.34.4

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "BiometricSDKFaceCapturePluginNormal",
     defaultLocalization: "es",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v11)
     ],
     products: [
         .library(
